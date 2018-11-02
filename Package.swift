81166efdfaeeaafee540879e886b4c991c8c00de
blueisle <lineart@gmail.com>
2018-11-02 13:47:44 +0900

    Update Package.swift
    update dependencies

--- Package.swift
+++ Package.swift
@@ -9,11 +9,11 @@ let package = Package(
         .library(name: "BivrostHelper", targets: ["BivrostHelper"])
     ],
     dependencies: [
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift", .upToNextMinor(from: "0.8.0")),
-        .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMajor(from: "3.0.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMinor(from: "7.0.2")),
-        .package(url: "https://github.com/Quick/Quick.git", .upToNextMinor(from: "1.2.0")),
-        .package(url: "https://github.com/kylef/Commander.git", .revision("e0cbee1bd73778c1076c675eaf660e97d09f3b32")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift", .upToNextMinor(from: "0.13.0")),
+        .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMajor(from: "3.1.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMinor(from: "7.3.1")),
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMinor(from: "1.3.0")),
+        .package(url: "https://github.com/kylef/Commander.git", .upToNextMinor(from: "0.8.0")),
         // PathKit fork supporting SPM4
         .package(url: "https://github.com/PoissonBallon/PathKit.git", .branch("master")),
         // Stencil fork supporting SPM4
